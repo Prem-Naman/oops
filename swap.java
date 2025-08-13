42eeaf94952b32af931077c46d60d25fd7f68579
Prem-Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-08-13 15:56:33 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        Scanner Input= new Scanner(System.in);
+        System.out.println("WELCOME TO SWAPPING \n \n");
+        System.out.println("enter a number :");
+        int a= Input.nextInt();
+        System.out.println("enter b number :");
+        int b=Input.nextInt();
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("after swapping4");
+        System.out.println("enter a number :"+a);
+        System.out.println("enter b number :"+b);
+
+
+
+
+
+    }
+}
