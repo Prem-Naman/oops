d944aa54fd7a744d8317b4af60155cf8fe35d81e
Prem Naman <178700626+Prem-Naman@users.noreply.github.com>
2025-11-01 15:12:00 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,11 @@
+class Example {
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        // Using length() method
+        int len = str.length();
+
+        System.out.println("String: " + str);
+        System.out.println("Length of the string: " + len);
+    }
+}
